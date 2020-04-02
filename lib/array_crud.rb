143e3a44c16cacde598ec9ffc4035bb452fabc97
Saida Davis <saihdavis@gmail.com>
2020-04-02 00:37:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   my_array = ["one", "two", "three", "four"]
   my_array << "five"
-  puts my_array.index
+  puts my_array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
