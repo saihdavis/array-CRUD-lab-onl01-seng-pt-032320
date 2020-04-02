0055771035248679b8b110599176e063a982b56a
Saida Davis <saihdavis@gmail.com>
2020-04-02 01:07:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   my_array = ["one", "two", "three", "four"]
-  my_array.unshift("arrays!")
+  my_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
