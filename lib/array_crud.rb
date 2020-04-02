925d605726b881c9bc1d3da16c00241523e0241e
Saida Davis <saihdavis@gmail.com>
2020-04-02 01:57:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 
 def remove_element_from_end_of_array(array)
   my_array = ["one", "two", "three", "four"]
-  four
+  four = my_array.pop
 end
 
 def remove_element_from_start_of_array(array)
